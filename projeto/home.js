cc5fbc5742f5aa63eba875ef1de3e1bc4c3b54a7
Mauricio Corradi <62272602+mauriciocorradi@users.noreply.github.com>
2020-03-29 17:46:38 -0300

    Add files via upload

--- projeto/home.js
+++ projeto/home.js
@@ -1,13 +1,25 @@
-var e = 15
+var e = 200
+
 function mudar(ponto){
     var n = document.getElementById('ponto')
-    
-    n.style.top = "250px"
     n.style.left = e + 'px'
-    e = e + 10
-    if (e > 570){
-        alert('Você venceu!')
+    e = e + 10    
+    if (e > 700){
+        alert('Verde venceu!')
     }
 }
 
+window.onload = initPage; //Inicia a função ao carregar a pagina.
+function initPage() { 
+    var f = 198
+    var m = document.getElementById('ponto01')
+      setInterval( function() {  //roda o loop sem parar
+      f=f+10
+      m.style.left = f + 'px';
+      if (f > 690) {
+        alert('Vermelho venceu!')
+    }
+                               }, 200 );
+  }
 
+  
